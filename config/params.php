a4a452a3e9bb74e3f5b1f46b2755188a7d38aea2
devanych <mail@devanych.ru>
2021-01-20 19:27:12 +0300

    Add configuration

---
+++ config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'yiisoft/yii-db-migration' => [
+        'createNamespace' => 'Yiisoft\Cache\Db\Migration',
+        'updateNamespace' => ['Yiisoft\Cache\Db\Migration'],
+    ],
+];
