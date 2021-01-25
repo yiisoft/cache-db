977c1542bad69b0c0369a86569bbdccecf445d27
devanych <mail@devanych.ru>
2021-01-25 17:33:24 +0300

    Remove redundant configuration

--- config/params.php
+++ config/params.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 return [
     'yiisoft/yii-db-migration' => [
-        'createNamespace' => 'Yiisoft\Cache\Db\Migration',
         'updateNamespace' => ['Yiisoft\Cache\Db\Migration'],
     ],
 ];
