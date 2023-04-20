e371a82f4197f47805ceaccded0dbba1ff82e782
Wilmer Arambula <terabytesoftw@gmail.com>
2023-04-20 11:43:43 -0400

    Fix oracle workflow, update config rector.

--- rector.php
+++ rector.php
@@ -7,16 +7,28 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
+    // paths
+    $rectorConfig->paths(
+        [
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+        ],
+    );
+
+    // skip paths
+    $rectorConfig->skip(
+        [
+            __DIR__ . '/config'
+        ],
+    );
 
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_80,
-    ]);
+    $rectorConfig->sets(
+        [
+            LevelSetList::UP_TO_PHP_80,
+        ],
+    );
 };
