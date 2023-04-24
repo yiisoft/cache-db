ddf8b283debb68c3a8e755b9753d61108cb6cfbe
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-04-24 18:27:14 -0400

    Update src/DbHelper.php
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/DbHelper.php
+++ src/DbHelper.php
@@ -30,11 +30,11 @@ final class DbHelper
         return true;
     }
 
-    public static function dropTable(ConnectionInterface $db, string $table, bool $force = false): void
+    public static function dropTable(ConnectionInterface $db, string $table): void
     {
         $command = $db->createCommand();
 
-        if ($db->getTableSchema($table, $force) !== null) {
+        if ($db->getTableSchema($table, true) !== null) {
             $command->dropTable($table)->execute();
         }
     }
