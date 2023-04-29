f33ed2d5a641002da27750d6cbc53e5ca5970fa2
Wilmer Arambula <terabytesoftw@gmail.com>
2023-04-29 09:00:33 -0400

    Add more tests.

--- tests/Common/AbstractMigrationTest.php
+++ tests/Common/AbstractMigrationTest.php
@@ -32,6 +32,27 @@ abstract class AbstractMigrationTest extends TestCase
      */
     public function testDropTable(): void
     {
+        Migration::ensureTable($this->db);
+
+        $this->assertNotNull($this->db->getTableSchema('{{%cache}}', true));
+
+        Migration::dropTable($this->db);
+
+        $this->assertNull($this->db->getTableSchema('{{%cache}}', true));
+
+        Migration::dropTable($this->db);
+
+        $this->assertNull($this->db->getTableSchema('{{%cache}}', true));
+    }
+
+    /**
+     * @throws Exception
+     * @throws InvalidConfigException
+     * @throws NotSupportedException
+     * @throws Throwable
+     */
+    public function testDropTableWithCustomTableName(): void
+    {
         Migration::ensureTable($this->db, '{{%custom_cache}}');
 
         $this->assertNotNull($this->db->getTableSchema('{{%custom_cache}}', true));
@@ -53,6 +74,23 @@ abstract class AbstractMigrationTest extends TestCase
      */
     public function testEnsureTable(): void
     {
+        Migration::ensureTable($this->db);
+
+        $this->assertNotNull($this->db->getTableSchema('{{%cache}}', true));
+
+        Migration::dropTable($this->db);
+
+        $this->assertNull($this->db->getTableSchema('{{%cache}}', true));
+    }
+
+    /**
+     * @throws Exception
+     * @throws InvalidConfigException
+     * @throws NotSupportedException
+     * @throws Throwable
+     */
+    public function testEnsureTableWithCustomTableName(): void
+    {
         Migration::ensureTable($this->db, '{{%custom_cache}}');
 
         $this->assertNotNull($this->db->getTableSchema('{{%custom_cache}}', true));
@@ -69,6 +107,27 @@ abstract class AbstractMigrationTest extends TestCase
      * @throws Throwable
      */
     public function testEnsureTableExist(): void
+    {
+        Migration::ensureTable($this->db);
+
+        $this->assertNotNull($this->db->getTableSchema('{{%cache}}'));
+
+        Migration::ensureTable($this->db, '{{%cache}}');
+
+        $this->assertNotNull($this->db->getTableSchema('{{%cache}}'));
+
+        Migration::dropTable($this->db);
+
+        $this->assertNull($this->db->getTableSchema('{{%cache}}', true));
+    }
+
+    /**
+     * @throws InvalidConfigException
+     * @throws NotSupportedException
+     * @throws Exception
+     * @throws Throwable
+     */
+    public function testEnsureTableExistWithCustomTableName(): void
     {
         Migration::ensureTable($this->db, '{{%custom_cache}}');
 
@@ -90,6 +149,32 @@ abstract class AbstractMigrationTest extends TestCase
      * @throws Throwable
      */
     public function testVerifyTableIndexes(): void
+    {
+        $dbCache = new DbCache($this->db, '{{%cache}}', 1_000_000);
+
+        Migration::ensureTable($this->db, $dbCache->getTable());
+
+        $schema = $this->db->getSchema();
+
+        /** @psalm-var IndexConstraint[] $indexes */
+        $indexes = $schema->getTableIndexes($dbCache->getTable(), true);
+
+        $this->assertSame(['id'], $indexes[0]->getColumnNames());
+        $this->assertTrue($indexes[0]->isUnique());
+        $this->assertTrue($indexes[0]->isPrimary());
+
+        Migration::dropTable($this->db, $dbCache->getTable());
+
+        $this->assertNull($this->db->getTableSchema($dbCache->getTable(), true));
+    }
+
+    /**
+     * @throws Exception
+     * @throws InvalidConfigException
+     * @throws NotSupportedException
+     * @throws Throwable
+     */
+    public function testVerifyTableIndexesWithCustomTableName(): void
     {
         $dbCache = new DbCache($this->db, '{{%custom_cache}}', 1_000_000);
 
@@ -116,6 +201,36 @@ abstract class AbstractMigrationTest extends TestCase
      * @throws Throwable
      */
     public function testVerifyTableStructure(): void
+    {
+        $dbCache = new DbCache($this->db, '{{%cache}}', 1_000_000);
+
+        Migration::ensureTable($this->db, $dbCache->getTable());
+
+        $prefix = $this->db->getTablePrefix();
+        $tableSchema = $this->db->getTableSchema($dbCache->getTable());
+
+        $this->assertNotNull($tableSchema);
+
+        $this->assertSame($prefix . 'cache', $tableSchema->getName());
+        $this->assertSame(['id'], $tableSchema->getPrimaryKey());
+        $this->assertSame(['id', 'data', 'expire'], $tableSchema->getColumnNames());
+        $this->assertSame(SchemaInterface::TYPE_STRING, $tableSchema->getColumn('id')?->getType());
+        $this->assertSame(128, $tableSchema->getColumn('id')?->getSize());
+        $this->assertSame(SchemaInterface::TYPE_BINARY, $tableSchema->getColumn('data')?->getType());
+        $this->assertSame(SchemaInterface::TYPE_INTEGER, $tableSchema->getColumn('expire')?->getType());
+
+        Migration::dropTable($this->db, $dbCache->getTable());
+
+        $this->assertNull($this->db->getTableSchema($dbCache->getTable(), true));
+    }
+
+    /**
+     * @throws Exception
+     * @throws InvalidConfigException
+     * @throws NotSupportedException
+     * @throws Throwable
+     */
+    public function testVerifyTableStructureWithCustomTableName(): void
     {
         $dbCache = new DbCache($this->db, '{{%custom_cache}}', 1_000_000);
 
