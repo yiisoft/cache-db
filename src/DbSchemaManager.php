554c930ea4c854d8805f5e5bcca4cdb797a34786
Wilmer Arambula <terabytesoftw@gmail.com>
2023-05-06 06:36:52 -0400

    Add phpdoc, minor improvements.

--- src/DbSchemaManager.php
+++ src/DbSchemaManager.php
@@ -5,19 +5,32 @@ declare(strict_types=1);
 namespace Yiisoft\Cache\Db;
 
 use Throwable;
+use Yiisoft\Db\Command\CommandInterface;
 use Yiisoft\Db\Connection\ConnectionInterface;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Exception\NotSupportedException;
 use Yiisoft\Db\Schema\SchemaInterface;
 
+/**
+ * Manages the cache table schema in the database.
+ */
 final class DbSchemaManager
 {
+    private CommandInterface $command;
+    private SchemaInterface $schema;
+
     public function __construct(private ConnectionInterface $db)
     {
+        $this->command = $db->createCommand();
+        $this->schema = $db->getSchema();
     }
 
     /**
+     * Ensures that the cache table exists in the database.
+     *
+     * @param string $table The name of the cache table. Defaults to '{{%yii_cache}}'.
+     *
      * @throws Exception
      * @throws NotSupportedException
      * @throws InvalidConfigException
@@ -25,39 +38,48 @@ final class DbSchemaManager
      */
     public function ensureTable(string $table = '{{%yii_cache}}'): void
     {
-        $schema = $this->db->getSchema();
-        $command = $this->db->createCommand();
-        $tableRawName = $schema->getRawTableName($table);
+        $tableRawName = $this->schema->getRawTableName($table);
 
         if ($this->hasTable($table)) {
             return;
         }
 
-        $command->createTable(
+        $this->command->createTable(
             $table,
             [
-                'id' => $schema->createColumn(SchemaInterface::TYPE_STRING, 128)->notNull(),
-                'data' => $schema->createColumn(SchemaInterface::TYPE_BINARY),
-                'expire' => $schema->createColumn(SchemaInterface::TYPE_INTEGER),
+                'id' => $this->schema->createColumn(SchemaInterface::TYPE_STRING, 128)->notNull(),
+                'data' => $this->schema->createColumn(SchemaInterface::TYPE_BINARY),
+                'expire' => $this->schema->createColumn(SchemaInterface::TYPE_INTEGER),
                 "CONSTRAINT [[PK_$tableRawName]] PRIMARY KEY ([[id]])",
             ],
         )->execute();
     }
 
     /**
+     * Ensures that the cache table does not exist in the database.
+     *
+     * @param string $table The name of the cache table. Defaults to '{{%yii_cache}}'.
+     *
      * @throws Exception
      * @throws InvalidConfigException
      * @throws Throwable
      */
     public function ensureNoTable(string $table = '{{%yii_cache}}'): void
     {
-        $tableRawName = $this->db->getSchema()->getRawTableName($table);
+        $tableRawName = $this->schema->getRawTableName($table);
 
         if ($this->hasTable($table)) {
-            $this->db->createCommand()->dropTable($tableRawName)->execute();
+            $this->command->dropTable($tableRawName)->execute();
         }
     }
 
+    /**
+     * Checks if the given table exists in the database.
+     *
+     * @param string $table The name of the table to check.
+     *
+     * @return bool Whether the table exists or not.
+     */
     private function hasTable(string $table): bool
     {
         return $this->db->getTableSchema($table, true) !== null;
